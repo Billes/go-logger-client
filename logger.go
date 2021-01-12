180664b88bb23d839be6265f22ca73b4311ee9cb
Andrée Björkgård <andree.bjorkgard@billes.se>
2021-01-12 16:13:14 +0100

    Revert "Merge pull request #1 from Billes/v2"
    This reverts commit 17de9bd4d7a13abba188863f4e9d4d0f365907d1, reversing
    changes made to 4a9f4840c45a64f4bfa487eca8b6a9190cf8ce48.

--- logger.go
+++ logger.go
@@ -4,21 +4,20 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/valyala/fasthttp"
 	coreLog "log"
 	"os"
 	"time"
-
-	"github.com/valyala/fasthttp"
 )
 
 type severity string
 
 const (
-	criticalSeverity severity = "CRITICAL"
-	debugSeverity    severity = "DEBUG"
-	errorSeverity    severity = "ERROR"
-	infoSeverity     severity = "INFO"
-	warningSeverity  severity = "WARNING"
+	CRITICAL severity = "CRITICAL"
+	DEBUG    severity = "DEBUG"
+	ERROR    severity = "ERROR"
+	INFO     severity = "INFO"
+	WARNING  severity = "WARNING"
 )
 
 type logEntry struct {
@@ -28,15 +27,10 @@ type logEntry struct {
 	Data     interface{} `json:"data"`
 }
 
-// Options is the config that is used for bootstrapping the logger.
-// Default is posting logs to remote server but omitting host will
-// write local logs instead.
 type Options struct {
-	Host    string `json:"host"`    // When omitting, logs will be written locally
-	System  string `json:"system"`  // Required
-	Token   string `json:"token"`   // Required if Host is set
-	Local   bool   `json:"local"`   // Default false - If you want to force local logs in addition to the remote ones
-	Timeout int    `json:"timeout"` // Default 10 - How long communication with server is allowed to take before giving up and writing a local log
+	Host   string
+	System string
+	Token  string
 }
 
 type logger struct {
@@ -44,79 +38,47 @@ type logger struct {
 }
 
 const format = "2006-01-02 15:04:05"
-const timeout = 10
-
-var logTags = []string{"logging"}
 
 var logr *logger
 
-// Init bootstraps the config to the logger instance
-func Init(o Options) error {
-	if o.Timeout < 1 {
-		o.Timeout = timeout
-	}
-
+func Init(o Options) {
 	if logr != nil {
-		err := errors.New("Trying to instantiate an already instantiated logger")
-		Error(logTags, err.Error(), nil)
-		return err
+		Error([]string{"logging"}, "Trying to instantiate an already instantiated logger", nil)
+		return
 	}
 
 	logr = &logger{o}
-
-	if o.Host == "" {
-		Warning(logTags, "Host is not set", nil)
-	}
-
-	return nil
 }
 
-// Critical creates a log for critical error messages.
-// Is synchronous and if you need concurrency run it as a goroutine.
 func Critical(tags []string, message string, data interface{}) {
-	log(newEntry(criticalSeverity, tags, message, data))
+	go log(newEntry(CRITICAL, tags, message, data))
 }
 
-// Debug creates a log for debug messages.
-// Is synchronous and if you need concurrency run it as a goroutine.
 func Debug(tags []string, message string, data interface{}) {
-	log(newEntry(debugSeverity, tags, message, data))
+	go log(newEntry(DEBUG, tags, message, data))
 }
 
-// Error creates a log for error messages.
-// Is synchronous and if you need concurrency run it as a goroutine.
 func Error(tags []string, message string, data interface{}) {
-	log(newEntry(errorSeverity, tags, message, data))
+	go log(newEntry(ERROR, tags, message, data))
 }
 
-// Fatal creates a log for critical error messages and shuts down the server.
-// Is synchronous and should not be ran concurrently as it would defeat the
-// purpose of being a fatal action.
 func Fatal(tags []string, message string, data interface{}) {
-	e := newEntry(criticalSeverity, tags, message, data)
-	if err := log(e); err == nil && !logr.options.Local {
-		// If an error didnt occur here, it wont write a local log so we do it here
+	e := newEntry(CRITICAL, tags, message, data)
+	if err := log(e); err == nil {
 		writeLocalLog(e)
 	}
 	os.Exit(1)
 }
 
-// Info creates a log for informational messages.
-// Is synchronous and if you need concurrency run it as a goroutine.
 func Info(tags []string, message string, data interface{}) {
-	log(newEntry(infoSeverity, tags, message, data))
+	go log(newEntry(INFO, tags, message, data))
 }
 
-// Warning creates a log for warning messages.
-// Is synchronous and if you need concurrency run it as a goroutine.
 func Warning(tags []string, message string, data interface{}) {
-	log(newEntry(warningSeverity, tags, message, data))
+	go log(newEntry(WARNING, tags, message, data))
 }
 
 func newEntry(severity severity, tags []string, message string, data interface{}) logEntry {
-	if logr == nil {
-		coreLog.Fatal("You need to instantiate the logger first")
-	}
 	return logEntry{
 		severity,
 		append([]string{logr.options.System}, tags...),
@@ -126,15 +88,19 @@ func newEntry(severity severity, tags []string, message string, data interface{}
 }
 
 func log(e logEntry) error {
+	if logr == nil {
+		coreLog.Fatal("You need to instantiate the logger first")
+	}
+
 	body, err := json.Marshal(e)
 	if err != nil {
 		writeLocalLog(e)
-		Error(logTags, fmt.Sprintf("Could not post to log due to \"data\" wasn't encodable - See local log"), "")
+		Error([]string{"logging"}, fmt.Sprintf("Could not post to log due to \"data\" wasn't encodable - See local log"), "")
 	}
 
 	if err == nil {
 		err = postLog(body)
-		if err != nil || logr.options.Local {
+		if err != nil {
 			writeLocalLog(e)
 		}
 		return err
@@ -157,11 +123,7 @@ func postLog(body []byte) error {
 	res := fasthttp.AcquireResponse()
 	client := &fasthttp.Client{}
 
-	err := client.DoTimeout(req, res, time.Duration(logr.options.Timeout)*time.Second)
-	if err != nil && logr.options.Host != "" {
-		entry := newEntry(warningSeverity, logTags, fmt.Sprintf("Failed while sending log entry request: %s", err.Error()), nil)
-		writeLocalLog(entry)
-	}
+	err := client.Do(req, res)
 	return err
 }
 
